023d0ea85fc00ee801e0396ab645d883936723e6
W1lden <Asan.Bitanov@gmail.com>
2025-06-12 16:33:16 +0500

    task 2, 3 fix

--- tasks/task2.py
+++ tasks/task2.py
@@ -1,11 +1,5 @@
-# def is_even(number: int) -> int:
-#     is_even = True
-#     if number % 2 != 0:
-#         is_even = False
-#     return is_even
-
 def is_even(number: int) -> int:
-    if number % 2 == 0:
-        return True
-    else:
+    if number % 2:
         return False
+    else:
+        return True
--- tasks/task3.py
+++ tasks/task3.py
@@ -1,4 +1,5 @@
 def reverse_string(string: str) -> str:
-    reverted_string = list(string)
-    reverted_string.reverse()
-    return "".join(reverted_string)
+    rev_str = ""
+    for s in range(1, len(string) + 1):
+        rev_str = rev_str + string[-s]
+    return rev_str
